dfb906fcbc0de54edaeed61bf52e65e9335a9a66
Remigijus <you@example.com>
2022-08-17 18:38:26 +0300

    task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+"use strict";
+console.log("script.js");
+
+class Calculator {
+  constructor(ai, be) {
+    this.a = ai;
+    this.b = be;
+  }
+  sum() {
+    return this.a + this.b;
+  }
+  subtraction() {
+    return this.a - this.b;
+  }
+  multiplication() {
+    return this.a * this.b;
+  }
+  division() {
+    return this.a / this.b;
+  }
+}
+
+console.log("new Calculator(12, 6).sum() ===", new Calculator(12, 6).sum());
+console.log("new Calculator(12, 6).subtraction() ===", new Calculator(12, 6).subtraction());
+console.log("new Calculator(12, 6).multiplication() ===", new Calculator(12, 6).multiplication());
+console.log("new Calculator(12, 6).division() ===", new Calculator(12, 6).division());
