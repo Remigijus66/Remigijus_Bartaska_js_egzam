4adfe3ff6aeb620b839086d783d35d4e2d920092
Remigijus <you@example.com>
2022-08-19 14:21:22 +0300

    task 1

--- Task 9/script.js
+++ Task 9/script.js
@@ -10,25 +10,21 @@ Metodas:
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
 "use strict";
-console.log("script.js");
-/**
- *
- * @param {string} ttl
- * @param {string} director
- * @param {number} budget
- */
-function Movie(ttl, director, budget) {
-  this.title = ttl;
-  this.director = director;
-  this.budget = budget;
 
-  this.wasExpensive = function () {
-    if (this.budget > 100000000) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+class Movie {
+  constructor(ttl, director, budget) {
+    this.title = ttl;
+    this.director = director;
+    this.budget = +budget;
+
+    this.wasExpensive = function () {
+      if (this.budget > 100000000) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }
 }
 const titanic = new Movie("Titanic", "Cameron", 180000000);
 console.log("titanic ===", titanic);
